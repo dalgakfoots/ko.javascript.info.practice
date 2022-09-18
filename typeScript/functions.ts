ebc878d539d84dfa17f0199ba0f9ba54496d91cc
FAS00223 <hsw3074@ninetree.com>
2022-09-19 08:11:48 +0900

    TypeScript : 함수

--- typeScript/functions.ts
+++ typeScript/functions.ts
@@ -20,3 +20,94 @@ let myAdd : (baseValue : number , increment : number) => number =
 
 
 /************************************************************/
+
+function _buildName(firstName: string, lastName = 'smith') {
+    if (lastName) return `${firstName} ${lastName}`;
+    else return firstName;
+}
+
+/************************************************************/
+
+function buildName(firstName: string, ...restOfName: string[]) {
+    return `${firstName} ${restOfName.join(" ")}`;
+}
+
+let buildNameFun : (fname : string , ...rest : string[]) => string = buildName;
+
+/************************************************************/
+
+let _deck = {
+    suits : ['hearts' , 'spades' , 'clubs' , 'diamonds'],
+    cards : Array(52),
+    createCardPicker : function () {
+         return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit : this.suits[pickedSuit] , card: pickedCard % 13};
+
+        }
+    }
+}
+
+let cardPicker = _deck.createCardPicker();
+let pickedCard = cardPicker();
+
+/************************************************************/
+
+interface Card {
+    suit : string;
+    card : number;
+}
+
+interface Deck {
+    suits : string[];
+    cards : number[];
+    createCardPicker(this : Deck) : () => Card;
+    // 명시적으로 선언된 this 매개변수는 함수의 매개변수 목록에서
+    // 가장 먼저 나오는 가짜 매개변수이다.
+}
+
+let deck : Deck = {
+    suits : ['hearts' , 'spades' , 'clubs' , 'diamonds'],
+    cards : Array(52),
+
+    createCardPicker: function (this : Deck) {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+/************************************************************/
+
+class Handler {
+    info : string;
+    _onClickGood(this: void , e : Event) {
+        console.log('clicked');
+    }
+
+    // onClickGood = (e : Event) => {this.info = e.message}
+}
+
+/************************************************************/
+
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+function pickCard(x : {suit : string; card : number;} []) : number;
+function pickCard(x: number): {suit : string ; card: number;} ;
+function pickCard(x) : any {
+
+    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
